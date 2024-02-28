05978327dace34cf1d23ebe8df09407c93aa5cbb
ArizCefDev <arizcefdev@gmail.com>
2024-02-28 16:50:59 +0400

    update

---
+++ DataAccess/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Score> Scores { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
--- TopazAPI/Program.cs
+++ TopazAPI/Program.cs
@@ -1,3 +1,6 @@
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DbContext
+builder.Services.AddDbContext<AppDBContext>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("DataConnection"))
+);
+
 
 var app = builder.Build();
 
